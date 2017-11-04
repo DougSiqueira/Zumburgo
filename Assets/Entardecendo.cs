b3cae6e604094e319c5652547e3bb9f5b292713d
Douglas <douglas.ms_@hotmail.com>
2017-11-04 17:02:20 -0200

    migrando para o GitHub

---
+++ Assets/Entardecendo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entardecendo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(10f * Time.deltaTime, 0, 0); // quanto menos o valor...mais demorado fica
+    }
+}
---
+++ Assets/Zombie/Scripts/Inimigo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))] //segue
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class Inimigo : MonoBehaviour
+{
+
+    private GameObject uChan;
+    private NavMeshAgent naveMesh;
+    private uChanController uChanControl;
+
+    private bool podeAtacar;
+
+
+    void Start()
+    {
+        podeAtacar = true;
+        uChan = GameObject.FindWithTag("Player"); //achar o player
+        uChanControl = uChan.GetComponent<uChanController>();
+        naveMesh = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, uChan.transform.position) >= 1.5f && Vector3.Distance(transform.position, uChan.transform.position) <= 10.0f)
+        {
+            naveMesh.destination = uChan.transform.position;
+        }
+
+        if (Vector3.Distance(transform.position, uChan.transform.position) < 1.5f)
+        {
+            Atacar();
+        }
+    }
+
+    void Atacar()
+    {
+        if (podeAtacar == true)
+        {
+            StartCoroutine("TempoDeAtaque");
+            uChanControl.Life -= 1; //vida provisoria
+        }
+    }
+    IEnumerator TempoDeAtaque()
+    {  // tempo entre ataques ao jogador
+        podeAtacar = false;
+        yield return new WaitForSeconds(1);
+        podeAtacar = true;
+
+    }
+
+
+}
